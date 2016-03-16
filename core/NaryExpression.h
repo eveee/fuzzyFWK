02fa6dac39f43d7f0553d76b8141337367bdabcf
Eva Ly Kok <evi.lyke@gmail.com>
2016-03-16 08:07:33 +0100

    Commit 9

---
+++ core/NaryExpression.h
@@ -0,0 +1,15 @@
+#ifndef NARYEXPRESSION_H_INCLUDED
+#define NARYEXPRESSION_H_INCLUDED
+
+#include "BinaryExpression.h"
+
+namespace core{
+
+    template <class T>
+    class NaryExpression{
+        public:
+            virtual T evaluate(Expression<T>[]*) const = 0;
+    };
+}
+
+#endif // NARYEXPRESSION_H_INCLUDED
---
+++ core/NaryShadowExpression.h
@@ -0,0 +1,56 @@
+#ifndef NARYSHADOWEXPRESSION_H_INCLUDED
+#define NARYSHADOWEXPRESSION_H_INCLUDED
+
+#include "NaryExpressionModel.h"
+
+namespace core{
+
+    template <class T>
+    class NaryShadowExpression : public NaryExpression<T>
+    {
+
+        private :
+
+            NaryExpression<T>* target;
+
+        public :
+
+            NaryShadowExpression(NaryExpression<T>*);
+            virtual T evaluate(Expression<T>[]*) const;
+            void setTarget(const NaryExpression<T>*);
+            NaryExpression<T>* getTarget() const;
+
+    };
+
+    template <class T>
+    NaryShadowExpression<T>::NaryShadowExpression(NaryExpression<T>* _target):
+        target(_target)
+	{
+
+	}
+
+    template <class T>
+    T NaryShadowExpression<T>::evaluate(Expression<T>[]* operands) const
+	{
+        if(target != NULL){
+            return target->evaluate(operands);
+        }
+        else
+        	throw NullExpressionException("the expression is null", __LINE__);
+    }
+
+    template <class T>
+    void NaryShadowExpression<T>::setTarget(const NaryExpression<T>* o)
+    {
+       	target = const_cast<NaryExpression<T>* >(o);
+    }
+
+    template <class T>
+    NaryExpression<T>* NaryShadowExpression<T>::getTarget() const
+    {
+       	return target;
+    }
+
+}
+
+#endif // NARYSHADOWEXPRESSION_H_INCLUDED
